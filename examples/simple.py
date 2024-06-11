2f6a09300f21b487d79bf7c3634348057eb74dcb
Clement de Sede <clement.desede@gmail.com>
2024-06-11 12:53:58 +0200

    Initial commit

---
+++ examples/simple.py
@@ -0,0 +1,156 @@
+from pynary import IOobject, IOStream, cast_object, types
+from dataclasses import dataclass, field
+from typing import TypeAlias
+
+@IOobject
+class Blob(IOStream):
+	size: types.INT_BE
+	text: types.BYTES_BE
+
+	@classmethod
+	def new(cls, *, text: str) -> 'Blob':
+		return cast_object(Blob, {'size': len(text), 'text': text.encode('utf8')})
+
+	@classmethod
+	def to(cls, b: 'Blob') -> str:
+		return b[1] # pyright: ignore
+
+	def read(self):
+		return self.size, self.text
+
+	def write(self, text: bytes):
+		self.size = types.INT_BE(len(text))
+		self.text = types.BYTES_BE(text)
+
+@IOobject
+class SBlob(IOStream):
+	size: types.INT_BE
+	text: types.STR_BE
+
+	@classmethod
+	def new(cls, *, text: str) -> 'SBlob':
+		return cast_object(SBlob, {'size': len(text), 'text': text.encode('utf8')})
+
+	@classmethod
+	def to(cls, b: 'SBlob') -> str:
+		return b[1] # pyright: ignore
+
+	def read(self):
+		return self.size, self.text
+
+	def write(self, text: str):
+		self.size = types.INT_BE(len(text))
+		self.text = types.STR_BE(text.encode('utf8'))
+
+@IOobject
+class Integer(IOStream):
+	N: types.INT_BE
+
+	@classmethod
+	def new(cls, n, **kwargs) -> 'Integer':
+		return cast_object(Integer, {'N': n})
+
+	def read(self):
+		return self.N
+
+	def write(self, n):
+		self.N = types.INT_BE(n)
+
+@IOobject
+class Field(IOStream):
+	start: types.CHAR_BE
+	case: types.CHAR_BE
+	space: types.CHAR_BE
+	key: SBlob
+
+	def read(self):
+		start = self.start
+		case = self.case
+		space = self.space
+		key = self.key
+		return case, SBlob.to(key)
+
+	def write(self, case: str, key):
+		self.start = types.CHAR_BE(b':')
+		self.case = types.CHAR_BE(case[0].encode('utf8'))
+		self.space = types.CHAR_BE(b' ')
+		self.key = SBlob.new(text=key)
+
+ParamValue: TypeAlias = int | str | bytes | list[str] | None
+
+@dataclass
+class parameter:
+	type: str
+	key: str
+	value: ParamValue
+
+@dataclass
+class structure:
+	parameters: list[parameter] = field(default_factory=list)
+	args: list[str] = field(default_factory=list)
+
+	def __getitem__(self, key):
+		for p in self.parameters:
+			if p.key == key:
+				return p
+		raise IndexError
+
+	def add_parameter(self, type: str, key: str, value: ParamValue):
+		self.parameters.append(parameter(type, key, value))
+		return self
+		
+	@classmethod
+	def read(cls, io):
+		self = cls()
+		while True:
+			f = Field(io)
+			case, key = f.read()
+			if case == 'i':
+				self.parameters.append(parameter(str(case), key, int(Integer(io).read())))
+			elif case == 'b':
+				self.parameters.append(parameter(str(case), key, Blob(io).read()[1]))
+			elif case == 'B':
+				self.parameters.append(parameter(str(case), key, str(SBlob(io).read()[1])))
+			elif case == 'l':
+				nargs = Integer(io).read()
+				self.parameters.append(parameter(str(case), key,
+					 [str(SBlob(io).read()[1]) for i in range(nargs)]
+				))
+			elif case == 'E':
+				break
+		return self
+
+	def write(self, io):
+		for p in self.parameters:
+			Field(io).write(p.type, p.key)
+			if p.type == 'i':
+				assert isinstance(p.value, int)
+				Integer(io).write(p.value)
+			if p.type == 'b':
+				assert isinstance(p.value, bytes)
+				Blob(io).write(p.value)
+			if p.type == 'B':
+				assert isinstance(p.value, str)
+				SBlob(io).write(p.value)
+			if p.type == 'l':
+				assert isinstance(p.value, list)
+				Integer(io).write(len(p.value))
+				for s in p.value:
+					assert isinstance(s, str)
+					SBlob(io).write(s)
+		Field(io).write("E", '\0')
+
+def read(path: str):
+	with open(path, 'rb') as f:
+		s = structure.read(f)
+		print(s['argv'])
+		print(s['stdin'])
+
+def save(path: str):
+	with open(path, 'wb') as f:
+		s = (structure()
+			.add_parameter('i', 'integer', 0)
+			.add_parameter('l', 'array', ["first element", "second", "and so", "forth"])
+			.add_parameter('b', 'bynary blob', b'some raw data')
+			.add_parameter('B', 'string blob', 'some less raw data')
+		).write(f)
---
+++ src/pynary/__init__.py
@@ -0,0 +1,210 @@
+import struct
+from typing import Any, Type, NewType, TypeAlias, BinaryIO
+from io import BytesIO
+from enum import Enum
+import inspect
+
+
+__all__ = [
+	"SVAL", 
+
+	"cast_object",
+	"IOStream",
+	"IOobject",
+
+	"types"
+]
+
+class SVAL:
+	_format: str = ''
+	_last: int = 0
+	_val: Any = None
+	def __get__(self, obj, objtype=None):
+		if self._format in ['>s', '<s', 's']:
+			bytes_ = obj._io.read(struct.calcsize(self._format) * SVAL._last)
+			SVAL._last = 0
+			self._val = SVAL._last
+			return bytes_.decode('utf8')
+		elif self._format in ['>p', '<p', 'p']:
+			bytes_ = obj._io.read(struct.calcsize(self._format) * SVAL._last)
+			SVAL._last = 0
+			self._val = SVAL._last
+			return bytes_
+		elif self._format in ['>c', '<c', 'c']:
+			bytes_ = obj._io.read(1)
+			SVAL._last = 0
+			self._val = SVAL._last
+			return bytes_.decode('utf8')
+		else:
+			bytes_ = obj._io.read(struct.calcsize(self._format))
+			SVAL._last = struct.unpack(self._format, bytes_)[0]
+			self._val = SVAL._last
+			return SVAL._last
+
+	def __set__(self, obj, value):
+		if self._format in ['>s', '<s', 's']:
+			obj._io.write(value)
+		elif self._format in ['>p', '<p', 'p']:
+			obj._io.write(value)
+		else:
+			obj._io.write(struct.pack(self._format, value))
+
+class _CHAR_BE(SVAL):		_format = '>c'
+class _BYTE_BE(SVAL):		_format = '>b'
+class _BOOL_BE(SVAL):		_format = '>?'
+class _SHORT_BE(SVAL):		_format = '>h'
+class _USHORT_BE(SVAL):		_format = '>H'
+class _INT_BE(SVAL):		_format = '>i'
+class _UINT_BE(SVAL):		_format = '>I'
+class _LONG_BE(SVAL):		_format = '>l'
+class _ULONG_BE(SVAL):		_format = '>L'
+class _LONGLONG_BE(SVAL):	_format = '>q'
+class _ULONGLONG_BE(SVAL):	_format = '>Q'
+class _FLOAT_BE(SVAL):		_format = '>f'
+class _DOUBLE_BE(SVAL):		_format = '>d'
+class _STR_BE(SVAL):		_format = '>s'
+class _BYTES_BE(SVAL):		_format = '>p'
+
+class _CHAR_LE(SVAL):		_format = '<c'
+class _BYTE_LE(SVAL):		_format = '<b'
+class _BOOL_LE(SVAL):		_format = '<?'
+class _SHORT_LE(SVAL):		_format = '<h'
+class _USHORT_LE(SVAL):		_format = '<H'
+class _INT_LE(SVAL):		_format = '<i'
+class _UINT_LE(SVAL):		_format = '<I'
+class _LONG_LE(SVAL):		_format = '<l'
+class _ULONG_LE(SVAL):		_format = '<L'
+class _LONGLONG_LE(SVAL):	_format = '<q'
+class _ULONGLONG_LE(SVAL):	_format = '<Q'
+class _FLOAT_LE(SVAL):		_format = '<f'
+class _DOUBLE_LE(SVAL):		_format = '<d'
+class _STR_LE(SVAL):		_format = '<s'
+class _BYTES_LE(SVAL):		_format = '>p'
+
+
+class types(Enum):
+	CHAR_BE      = NewType('CHAR_BE', bytes)
+	BYTE_BE      = NewType('BYTE_BE', bytes)
+	BOOL_BE      = NewType('BOOL_BE', bool)
+	SHORT_BE     = NewType('SHORT_BE', int)
+	USHORT_BE    = NewType('USHORT_BE', int)
+	INT_BE       = NewType('INT_BE', int)
+	UINT_BE      = NewType('UINT_BE', int)
+	LONG_BE      = NewType('LONG_BE', int)
+	ULONG_BE     = NewType('ULONG_BE', int)
+	LONGLONG_BE  = NewType('LONGLONG_BE', int)
+	ULONGLONG_BE = NewType('ULONGLONG_BE', int)
+	FLOAT_BE     = NewType('FLOAT_BE', float)
+	DOUBLE_BE    = NewType('DOUBLE_BE', float)
+	STR_BE       = NewType('STR_BE', bytes)
+	BYTES_BE     = NewType('BYTES_BE', bytes)
+
+	CHAR_LE      = NewType('CHAR_LE', bytes)
+	BYTE_LE      = NewType('BYTE_LE', bytes)
+	BOOL_LE      = NewType('BOOL_LE', bool)
+	SHORT_LE     = NewType('SHORT_LE', int)
+	USHORT_LE    = NewType('USHORT_LE', int)
+	INT_LE       = NewType('INT_LE', int)
+	UINT_LE      = NewType('UINT_LE', int)
+	LONG_LE      = NewType('LONG_LE', int)
+	ULONG_LE     = NewType('ULONG_LE', int)
+	LONGLONG_LE  = NewType('LONGLONG_LE', int)
+	ULONGLONG_LE = NewType('ULONGLONG_LE', int)
+	FLOAT_LE     = NewType('FLOAT_LE', float)
+	DOUBLE_LE    = NewType('DOUBLE_LE', float)
+	STR_LE       = NewType('STR_LE', bytes)
+	BYTES_LE     = NewType('BYTES_LE', bytes)
+
+_GETTERS = {
+	types.CHAR_BE:      _CHAR_BE,
+	types.BYTE_BE:      _BYTE_BE,
+	types.BOOL_BE:      _BOOL_BE,
+	types.SHORT_BE:     _SHORT_BE,
+	types.USHORT_BE:    _USHORT_BE,
+	types.INT_BE:       _INT_BE,
+	types.UINT_BE:      _UINT_BE,
+	types.LONG_BE:      _LONG_BE,
+	types.ULONG_BE:     _ULONG_BE,
+	types.LONGLONG_BE:  _LONGLONG_BE,
+	types.ULONGLONG_BE: _ULONGLONG_BE,
+	types.FLOAT_BE:     _FLOAT_BE,
+	types.DOUBLE_BE:    _DOUBLE_BE,
+	types.STR_BE:       _STR_BE,
+	types.BYTES_BE:     _BYTES_BE,
+
+	types.CHAR_LE:      _CHAR_LE,
+	types.BYTE_LE:      _BYTE_LE,
+	types.BOOL_LE:      _BOOL_LE,
+	types.SHORT_LE:     _SHORT_LE,
+	types.USHORT_LE:    _USHORT_LE,
+	types.INT_LE:       _INT_LE,
+	types.UINT_LE:      _UINT_LE,
+	types.LONG_LE:      _LONG_LE,
+	types.ULONG_LE:     _ULONG_LE,
+	types.LONGLONG_LE:  _LONGLONG_LE,
+	types.ULONGLONG_LE: _ULONGLONG_LE,
+	types.FLOAT_LE:     _FLOAT_LE,
+	types.DOUBLE_LE:    _DOUBLE_LE,
+	types.STR_LE:       _STR_LE,
+	types.BYTES_LE:     _BYTES_LE,
+}
+
+
+def cast_object[T](type_: Type[T], values) -> T:
+	if isinstance(values, (list, list)):
+		for e in values:
+			if not isinstance(e, (tuple, list)):
+				raise TypeError("values must be a dict of property and values or a list that can be casted to such a dict")
+		values = dict(values)
+	if not isinstance(values, dict):
+		raise TypeError("values must be a dict of property and values or a list that can be casted to such a dict")
+	return values # pyright: ignore
+
+
+class IOStream:
+	def __init__(self, _io: BinaryIO):
+		self._io = _io
+		annotations = inspect.get_annotations(type(self))
+		for k, t in annotations.items():
+			if isinstance(t, type) and issubclass(t, IOStream):
+				setattr(self, k, t(self._io))
+
+
+class _descr:
+	def __init__(self, _type):
+		self._type = _type
+		self._instance = None
+
+	def __get__(self, obj, objtype=None):
+		return [getattr(self._instance, i) for i in self._type._getters]
+
+	def __set__(self, obj, values):
+		if isinstance(values, self._type):
+			self._instance = values
+			return 
+		for k in self._type._getters:
+			setattr(self._instance, k, values[k])
+
+
+def IOobject(c):
+	annotations = inspect.get_annotations(c)
+	c._is_ioobject = True
+	c._getters = []
+	for k, t in annotations.items():
+		if t in _GETTERS:
+			setattr(c, k, _GETTERS[t]())
+			c._getters.append(k)
+		if hasattr(t, '_is_ioobject') and t._is_ioobject:
+			setattr(c, k, _descr(t))
+
+	def __get__(self, obj, objtype=None):
+		for i in self._getters:
+			yield getattr(self, i)
+
+	def __set__(self, obj, values):
+		for k in self._getters:
+			setattr(self, k, values[k])
+
+	c.__get__ = __get__
+	c.__set__ = __set__
+	return c
